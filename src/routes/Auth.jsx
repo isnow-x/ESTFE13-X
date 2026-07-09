b5c8a08a5b4dde775fd68ffdb105f0edf2c42013
isnow-x <cone2119@naver.com>
2026-07-09 11:30:05 +0900

    feat: 이메일 비밀번호 회원가입 기능 추가

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Box, Typography, TextField, Button } from "@mui/material";
+import { authService } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
@@ -14,13 +16,36 @@ function Auth() {
       [name]: value,
     }));
   };
+
+  const auth = authService;
+
   console.log(form);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (newAccount) {
+      // 회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      // 로그인
+    }
+  };
   return (
     <>
       <Typography variant="h2" component="h2">
-        Login Form
+        {newAccount ? "회원가입 폼" : "로그인 폼"}
       </Typography>
-      <Box component="form" sx={{ mt: 2 }}>
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label="Email address"
@@ -39,7 +64,7 @@ function Auth() {
           onChange={handleChange}
         />
         <Button sx={{ mt: 2 }} type="submit" variant="contained">
-          LOGIN
+          {newAccount ? "회원가입" : "로그인"}
         </Button>
       </Box>
     </>
