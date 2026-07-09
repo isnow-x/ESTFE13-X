b6ea1770a12c6d53b109ae96d53b362f4b8e50c6
isnow-x <cone2119@naver.com>
2026-07-09 11:08:45 +0900

    feat: 이메일, 패스워드 이용해서 로그인 폼 생성

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import { Box, Typography, TextField, Button } from "@mui/material";
+
 function Auth() {
+  const [newAccount, setNewAccount] = useState(true);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setForm(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  console.log(form);
   return (
     <>
-      <h2>Auth</h2>
+      <Typography variant="h2" component="h2">
+        Login Form
+      </Typography>
+      <Box component="form" sx={{ mt: 2 }}>
+        <TextField
+          fullWidth
+          label="Email address"
+          type="text"
+          name="email"
+          variant="outlined"
+          onChange={handleChange}
+        />
+        <TextField
+          sx={{ mt: 2 }}
+          fullWidth
+          label="Password"
+          type="password"
+          name="password"
+          variant="outlined"
+          onChange={handleChange}
+        />
+        <Button sx={{ mt: 2 }} type="submit" variant="contained">
+          LOGIN
+        </Button>
+      </Box>
     </>
   );
 }
