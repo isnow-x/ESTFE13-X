2b8a56235782e6c32fc9b760fee57f28790c721f
isnow-x <cone2119@naver.com>
2026-07-15 11:11:21 +0900

    feat: 이미지 첨부 여부 분기

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -21,7 +21,7 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { db, storageService } from "../firebase";
-import { ref, uploadString } from "firebase/storage";
+import { ref, uploadString, getDownloadURL } from "firebase/storage";
 import { useEffect, useState, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Comments from "../components/Comments";
@@ -58,24 +58,27 @@ function Home({ userId }) {
   };
   const onSubmit = async e => {
     e.preventDefault();
-    const storageRef = ref(storage, `${userId}/${uuidv4()}`);
 
-    uploadString(storageRef, attachment, "data_url").then(snapshot => {
-      console.log("파일 업로드 완료");
-    });
-
-    // try {
-    //   const docRef = await addDoc(collection(db, "comments"), {
-    //     // comment: comment,
-    //     comment,
-    //     date: serverTimestamp(),
-    //     uid: userId,
-    //   });
-    //   setComment("");
-    //   // getComments();
-    // } catch (e) {
-    //   console.error("글 추가시 에러가 발생했습니다.", e);
-    // }
+    try {
+      let imageURL = null;
+      if (attachment) {
+        // 첨부파일이 있으면
+        const storageRef = ref(storage, `${userId}/${uuidv4()}`);
+        const snapshot = await uploadString(storageRef, attachment, "data_url");
+        imageURL = await getDownloadURL(storageRef); // 다운로드할 이미지 절대 경로 할당
+      }
+      const data = {
+        comment,
+        date: serverTimestamp(),
+        uid: userId,
+        image: imageURL,
+      };
+      const docRef = await addDoc(collection(db, "comments"), data); // firestore에 글 저장
+      setComment("");
+      onClearFile();
+    } catch (e) {
+      console.log("글 추가시 에러가 발생했습니다.");
+    }
   };
   const onFileChange = e => {
     const file = e.target.files[0];
